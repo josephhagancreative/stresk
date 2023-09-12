58391c568a4b34c6c835c67f9966809463b2cf4e
Joe Hagan <jhaganfilm@gmail.com>
2023-09-13 08:59:34 +0900

    filled in temp content for login and contact pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,17 @@
-import React from "react"
+import Link from "next/link"
 
 function Contact() {
-  return <h1>Contact</h1>
+  return (
+    <div className="flex justify-center flex-col mx-auto items-center min-h-screen bg-gradient-to-r from-purplebg to-blackbg">
+      <Link href="https://twitter.com/ImJoee">
+        <a target="_blank">
+          <h2 className="text-white text-xl cursor-pointer hover:underline">
+            Contact me on Twitter/X @ImJoee
+          </h2>
+        </a>
+      </Link>
+    </div>
+  )
 }
 
 export default Contact
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+function login() {
+  return (
+    <div className="flex justify-center flex-col mx-auto items-center min-h-screen bg-gradient-to-r from-purplebg to-blackbg">
+      <h2 className="text-white text-xl">
+        Login and User Profiles planned for the future
+      </h2>
+    </div>
+  )
+}
+
+export default login
